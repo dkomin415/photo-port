0edf492a35efa4a069a337e13b02e2b9c740f8b0
dkomin415 <dkomin415.sales@gmail.com>
2022-10-23 21:20:02 -0700

    ran tests for ContactForm

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('Contact component', () => {
+  // baseline test
+  it('renders', () => {
+    render(<ContactForm/>);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<ContactForm/>);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('shows text content', () => {
+  it('text matches text contect', () => {
+    const { getByTestId } = render(<ContactForm/>);
+
+    expect(getByTestId('contact')).toHaveTextContent('Contact me');
+
+    expect(getByTestId('submit')).toHaveTextContent('Submit');
+  })
+})
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -35,7 +35,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="contact">Contact me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor='name'>Name:</label>
@@ -54,7 +54,7 @@ function ContactForm() {
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button type="submit" onSubmit={handleSubmit}>Submit</button>
+        <button data-testid="submit" type="submit" onSubmit={handleSubmit}>Submit</button>
       </form>
     </section>
   )
